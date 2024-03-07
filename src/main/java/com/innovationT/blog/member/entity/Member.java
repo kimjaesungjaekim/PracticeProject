1620f94896c692cb81700c4db7d4edbac05a8816
kimjaesung145@gmail.com <kimjaesung145@gmail.com>
2024-03-07 12:59:39 +0900

    <로그인 구현 완료 >

--- src/main/java/com/innovationT/blog/member/entity/Member.java
+++ src/main/java/com/innovationT/blog/member/entity/Member.java
@@ -5,9 +5,6 @@ import javax.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.RequiredArgsConstructor;
-
-
 
 @Data
 @Builder
